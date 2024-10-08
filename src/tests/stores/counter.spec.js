8f65b48a76b7dc5f21d00aef9ae46c8a6eda1165
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-10-07 21:12:35 -0300

    M7 PRUEBA: Updated counter.spec.js to initialize Pinia in beforeEach and added assertions for initial count value and increment/decrement functionality

--- src/tests/stores/counter.spec.js
+++ src/tests/stores/counter.spec.js
@@ -3,26 +3,30 @@ import { createPinia, setActivePinia } from "pinia";
 import { useCounterStore } from "@/stores/counter";
 
 describe("useCounter", () => {
+    // Antes de cada prueba inicializo una nueva instancia de Pinia
     beforeEach(() => {
-        setActivePinia(createPinia());
+        setActivePinia(createPinia())
     })
 
-    // Verificar que se reciba un valor inicial en el contador
+    // Verifico que el contador inicial sea 0
     it("Contador inicial en 0", () => {
         const counterStore = useCounterStore()
-
+        // Afirmo que el valor inicial del contador es 0
         expect(counterStore.count).toBe(0)
     })
 
-    // Probar la funcionalidad de incremento del contador.
+    // Pruebo la funcionalidad de incremento y decremento del contador
     it("El contador aumenta", () => {
         const counterStore = useCounterStore()
 
+        // Incremento el contador
         counterStore.increment()
+        // Verifico que el contador sea 1 después de incrementar
         expect(counterStore.count).toBe(1)
 
+        // Decremento el contador
         counterStore.decrement()
+        // Verifico que el contador vuelva a ser 0 después de decrementar
         expect(counterStore.count).toBe(0)
     })
-
 })
